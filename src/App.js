fd2b3116e74c6ec58343ab72672ac6ff573f9f43
Ehsan <139962810+Ehsan-Rabobi@users.noreply.github.com>
2024-08-13 18:40:13 +0330

    Creating a theme folder in folder styles and writing a color theme

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, Container, ThemeProvider } from "@mui/material";
+import { useEffect } from "react";
+import theme from "./styles/theme";
 
 function App() {
+  useEffect(() => {
+    document.title = "React Material UI - Home";
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <ThemeProvider theme={theme}>
+        <Container
+          maxWidth="xl"
+          sx={{
+            background: "#fff",
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Button variant="contained">text</Button>
+        </Container>
+      </ThemeProvider>
+    </>
   );
 }
 
