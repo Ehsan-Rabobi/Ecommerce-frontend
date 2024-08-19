ee063608dc5577bb91dceaed5d466b5f12476aae
Ehsan <139962810+Ehsan-Rabobi@users.noreply.github.com>
2024-08-19 18:25:11 +0330

    complete banner and responsive in every size

--- src/components/banner/index.js
+++ src/components/banner/index.js
@@ -1,6 +1,6 @@
 import { Typography, useMediaQuery } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
-import { BannerContainer, BannerContent } from "../../styles/banner";
+import { BannerContainer, BannerContent, BannerDescription, BannerImage, BannerTitle } from "../../styles/banner";
 
 export default function Banner() {
   const theme = useTheme();
@@ -8,8 +8,16 @@ export default function Banner() {
   return (
     <>
       <BannerContainer>
+        <BannerImage src="/images/banner/banner.png"/>
         <BannerContent>
           <Typography variant="h6">Huge Collection</Typography>
+          <BannerTitle variant="h2">
+            New Bags
+          </BannerTitle>
+        <BannerDescription variant="subtitle">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam a tempore
+        expedita provident omnis animi odio reprehenderit dolor eius aliquam.
+        </BannerDescription>
         </BannerContent>
       </BannerContainer>
     </>
--- src/styles/banner/index.js
+++ src/styles/banner/index.js
@@ -1,6 +1,6 @@
-import { Box } from "@mui/system";
 import { styled } from "@mui/material/styles";
 import { Colors } from "../theme";
+import { Typography,Box } from "@mui/material";
 
 export const BannerContainer = styled(Box)(({ theme }) => ({
   display: "flex",
@@ -15,16 +15,42 @@ export const BannerContainer = styled(Box)(({ theme }) => ({
   },
 }));
 
+export const BannerImage = styled('img')(({src , theme})=>({
+  src:`url(${src})`,
+  width:"500px",
+  [theme.breakpoints.down('md')]:{
+    width:"350px"
+  },
+  [theme.breakpoints.down('sm')]:{
+    width:"320px",
+    height:"300px"
+  }
+}))
+
 export const BannerContent = styled(Box)(() => ({
     display: "flex",
     justifyContent: "center",
     flexDirection:"column",
     maxWidth:420,
-    padding:"30px"
+    padding:"30px",
 }))
 
-export const BannerTitle = styled(Typography)(()=>({
+export const BannerTitle = styled(Typography)(({theme})=>({
   lineHeight:1.5,
   fontSize:"72px",
-  marginBottom:"20px"
+  marginBottom:"20px",
+  [theme.breakpoints.down('sm')]:{
+    fontSize:"42px"
+  }
+}))
+
+export const BannerDescription = styled(Typography)(({theme})=>({
+  lineHeight:1.25,
+  marginBottom:"3em",
+  letterSpacing:1.25,
+  [theme.breakpoints.down('md')]:{
+    lineHeight:1.15,
+    marginBottom:"1.5em",
+    letterSpacing:1.15,
+  }
 }))
