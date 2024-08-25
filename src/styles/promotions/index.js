9d97936fdef2178bdbb47df6ecfdaeedb2672b97
Ehsan <139962810+Ehsan-Rabobi@users.noreply.github.com>
2024-08-25 10:09:27 +0330

    Correcting a small mistake in the promotion style

--- src/styles/promotions/index.js
+++ src/styles/promotions/index.js
@@ -1,16 +1,17 @@
-import { styled } from "@mui/system";
+import { styled , Box} from "@mui/system";
 import { Colors } from "../theme";
 import { Typography } from "@mui/material";
 
-export const PromotionsContainer = styled(Box)((theme)=>({
+export const PromotionsContainer = styled(Box)(({theme})=>({
     [theme.breakpoints.up('md')]:{
-        display:'flex',
-        justifyContent : 'center' , 
-        alignItems : 'center',
-        padding:"20px 0px 20px 0px",
-        overflow:"hidden",
-        background:Colors.secondary
-    }
+        padding:"40px 0px 40px 0px"
+    },
+    display:'flex',
+    justifyContent : 'center' , 
+    alignItems : 'center',
+    padding:"20px 0px 20px 0px",
+    overflow:"hidden",
+    background:Colors.secondary
 }))
 
 export const MessageText = styled(Typography)(({theme})=>({
